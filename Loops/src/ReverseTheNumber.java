22c1b4712a1e7ebc34fb8c3152534d7404bf843a
62062-tanesh <tanesh8409@gmail.com>
2025-07-25 21:40:15 +0530

    Add New Programs

---
+++ Loops/src/ReverseTheNumber.java
@@ -0,0 +1,2 @@
+public class ReverseTheNumber {
+}
---
+++ Loops/src/SumOfDigits.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int n = sc.nextInt();
+        while(n>0){
+            sum += n%10;
+            n = n/10;
+        }
+        System.out.println("The Sum of The Digits are: "+sum);
+    }
+}
