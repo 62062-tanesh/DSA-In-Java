ec9369de6a04459dcdd7a56d72c8d4986bf785a7
62062-tanesh <tanesh8409@gmail.com>
2025-07-29 00:36:07 +0530

    Added New Question

--- NestedLoop/src/NumberRectangularPattern.java
+++ NestedLoop/src/NumberRectangularPattern.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class RectangularPattern {
+public class NumberRectangularPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int  i=0;i<n;i++){
-            for(int j=0;j<n*2;j++){
-                System.out.print("* ");
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=n+2;j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
--- NestedLoop/src/PyramidPattern.java
+++ NestedLoop/src/PyramidPattern.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
-public class RectangularPattern {
+public class PyramidPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int  i=0;i<n;i++){
-            for(int j=0;j<n*2;j++){
+        for(int i = 1; i<=n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print("  ");
+            }
+            for(int k=0;k<2*i-1;k++){
                 System.out.print("* ");
             }
             System.out.println();
