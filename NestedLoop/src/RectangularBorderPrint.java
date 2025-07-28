646a44cc97998ffb6277591c8076f51781c55985
62062-tanesh <tanesh8409@gmail.com>
2025-07-28 22:17:48 +0530

    Added new Questions

--- NestedLoop/src/RectangularBorderPrint.java
+++ NestedLoop/src/RectangularBorderPrint.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class RectangularBorderPrint {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i=0;
+        while(i<n){
+            int j=0;
+            while(j<n*2){
+                if(j == 0 || j == (n*2)-1 || i == 0 || i == n-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print("#");
+                }
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
 }
---
+++ NestedLoop/src/TriangularPrint.java
@@ -0,0 +1,5 @@
+public class TriangularPrint {
+    public static void main(String[] args) {
+        
+    }
+}
