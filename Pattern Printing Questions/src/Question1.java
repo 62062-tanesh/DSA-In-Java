36ecff913c10047834c430dc5989d7275f311a57
62062-tanesh <tanesh8409@gmail.com>
2025-09-26 23:41:41 +0530

    Updatee the same  Question

--- Pattern Printing Questions/src/Question1.java
+++ Pattern Printing Questions/src/Question1.java
@@ -3,16 +3,11 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A Number: ");
         int n = sc.nextInt();
-        int sum = 0;
-        while(n>0){
-            sum = sum + (n%10);
-            n = n/10;
-        }
-        System.out.println("The Add is: "+sum);
+        int ans = reverse(n);
+        System.out.println("The Reverse is: "+ans);
 
-        int product = productofNum(n);
-        System.out.println("The Product is: "+product);
     }
 
     public static int productofNum (int n){
@@ -22,5 +17,24 @@ public class Question1 {
             n = n/10;
         }
         return ans;
+    }
+
+
+    public static void evenNums(int n){
+        while(n>0){
+            if((n%10)%2 == 0){
+                System.out.println(n%10);
+            }
+            n = n/10;
+        }
+    }
+
+    public static int reverse(int n){
+        int count = 0;
+        while(n>0){
+            count = (count * 10) + (n%10);
+            n = n/10;
+        }
+        return count;
     }
 }
