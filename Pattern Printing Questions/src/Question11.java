a281324da3157442b80561e050eeb9c1be97fffe
62062-tanesh <tanesh8409@gmail.com>
2025-10-11 02:25:42 +0530

    Added New Question

---
+++ Pattern Printing Questions/src/Question11.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Question11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A Number: ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            if(i%2 == 0){
+                for(int j=1; j<=i; j++){
+                    System.out.print((char)(j+64)+" ");
+                }
+                System.out.println();
+            }else{
+                for(int j=1; j<=i; j++){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
