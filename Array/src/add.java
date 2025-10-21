0791bf150c4e74eabdf9fec8d8b7c4bbb88993e0
62062-tanesh <tanesh8409@gmail.com>
2025-10-21 23:29:27 +0530

    Adding a Question

--- Array/src/add.java
+++ Array/src/add.java
@@ -1,7 +1,13 @@
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
 public class add {
     public static void main(String[] args) {
-        int n1 = 6;
-        int n2 = 7;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter Second Number: ");
+        int n2 = sc.nextInt();
         int sum = n1+n2;
         System.out.println(sum);
     }
